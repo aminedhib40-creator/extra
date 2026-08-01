423cca551f9820b4448812f98134257501e78283
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2026-08-01 11:15:26 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// # extra
+
+
+// 1- Crée une fonction qui prend une heure (0–23) et retourne :
+// "matin" si < 12
+// "après-midi" si entre 12 et 18
+// "soir" si > 18
+
+
+// 2- Crée une fonction qui prend un score et retourne :
+// "excellent" si score >= 90
+// "bon" si score >= 70
+// "moyen" si score >= 50
+// "faible" sinon
+
+
+// 3- Crée une fonction qui prend trois nombres et retourne le **plus petit** des trois
+
+// **sans utiliser Math.min**
+
+
+// 4- Crée une fonction qui prend un nombre et retourne "positif", "négatif" ou "zéro" selon sa valeur.
+
+
+
+// 5- Crée une fonction qui prend un nombre et retourne "divisible par 3", "divisible par 5" ou "autre" selon le cas.
+
+
+
+// 6- Crée une fonction qui prend deux nombres et retourne le plus grand des deux.
+
+
+
+// 7- Crée une fonction qui prend une note (0 à 20) et retourne "réussi" si la note est >= 10, sinon "échoué".
